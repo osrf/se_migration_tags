f0d9fb4449fe17b1348ab919baabfdbb98f0e7cb
Tully Foote <tullyfoote@intrinsic.ai>
2023-06-14 14:45:49 -0700

    new manual tags for ros

---
+++ reference/reprocess.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+lines = open('rosanswers_tags2.csv').readlines()
+out = []
+for l in lines:
+    elements = l.strip().split(',')
+    if len(elements) == 1:
+        orig = elements[0]
+        lower = orig.lower()
+        out.append(f'{orig},{lower}')
+    else:
+        out.append(l.strip())
+
+print(out)
+out.sort()
+with open('result.csv','w') as outh:
+    for l in out:
+        outh.write(f'{l}\n')
