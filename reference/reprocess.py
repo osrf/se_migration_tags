4b0aaca2ff7e742729a9a6525b7506539d330476
Tully Foote <tullyfoote@intrinsic.ai>
2023-06-16 08:29:47 -0700

    strip hard coded ros tags

--- reference/reprocess.py
+++ reference/reprocess.py
@@ -10,8 +10,6 @@ for l in lines:
         lower = orig.lower()
         out.append(f'{orig},{lower},ros')
     else:
-        if not 'ros' in elements[1:]:
-            elements.append('ros')
         out.append(','.join(elements))
 
 # print(out)
